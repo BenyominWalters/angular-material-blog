3a9cae1c3896fddf442e5b6d933053bdb7f35401
Benjamin Walters <31862759+riversplitter@users.noreply.github.com>
2018-06-01 15:22:37 -0500

    Got routes to work. Changed with Angular6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'dashboard', component: DashboardComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    MaterialModule,
+    FlexLayoutModule,
   ],
+
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
